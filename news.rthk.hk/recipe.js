c69ea84866188d8a77166a88c3b7bccf9c949e22
Sean Ho <seanho@thoughtworks.com>
2015-08-08 21:52:28 +1000

    Initial commit

---
+++ news.rthk.hk/recipe.js
@@ -0,0 +1,61 @@
+var cheerio = require('cheerio');
+var shrimp = require('shrimp');
+
+function getCategories() {
+  var categories = [
+    {title: '全部', navigate: function () { getArticles('http://news.rthk.hk/rthk/webpageCache/services/loadModNewsShowSp2List.php?lang=zh-TW&cat=NULL&newsCount=60&dayShiftMode=1') }},
+    {title: '本地', navigate: function () { getArticles('http://news.rthk.hk/rthk/webpageCache/services/loadModNewsShowSp2List.php?lang=zh-TW&cat=3&newsCount=60&dayShiftMode=1') }},
+    {title: '大中華', navigate: function () { getArticles('http://news.rthk.hk/rthk/webpageCache/services/loadModNewsShowSp2List.php?lang=zh-TW&cat=2&newsCount=60&dayShiftMode=1') }},
+    {title: '國際', navigate: function () { getArticles('http://news.rthk.hk/rthk/webpageCache/services/loadModNewsShowSp2List.php?lang=zh-TW&cat=4&newsCount=60&dayShiftMode=1') }},
+    {title: '財經', navigate: function () { getArticles('http://news.rthk.hk/rthk/webpageCache/services/loadModNewsShowSp2List.php?lang=zh-TW&cat=5&newsCount=60&dayShiftMode=1') }},
+    {title: '體育', navigate: function () { getArticles('http://news.rthk.hk/rthk/webpageCache/services/loadModNewsShowSp2List.php?lang=zh-TW&cat=6&newsCount=60&dayShiftMode=1') }}
+  ]
+  shrimp.show({items: categories});
+}
+
+function getArticles(url) {
+  shrimp.request(url, function (error, response, body) {
+    if (error) {
+      shrimp.alert(error.message);
+      return;
+    }
+
+    var $ = cheerio.load(body);
+    shrimp.debug(body);
+
+    var articles = $('.ns2-page').map(function () {
+      var url = $(this).find('a').attr('href').trim();
+      return {
+        title: $(this).find('.ns2-title').text().trim(),
+        date: $(this).find('.ns2-created').text().trim(),
+        navigate: function () {
+          getArticle(url);
+        }
+      };
+    }).get();
+
+    shrimp.show({items: articles});
+  });
+}
+
+function getArticle(url) {
+  shrimp.request(url, function (error, response, body) {
+    if (error) {
+      shrimp.alert(error.message);
+      return;
+    }
+
+    var $ = cheerio.load(body);
+
+    var content = $('.itemFullText').html();
+    var article = {
+      title: $('.itemTitle').text().trim(),
+      content: content,
+      baseUrl: url
+    };
+
+    shrimp.show({item: article});
+  });
+}
+
+shrimp.setEntry(getCategories);
---
+++ teepr.com/recipe.js
@@ -0,0 +1,85 @@
+var cheerio = require('cheerio');
+var shrimp = require('shrimp');
+
+function getCategories() {
+  shrimp.request('http://www.teepr.com', function (error, response, body) {
+    if (error) {
+      shrimp.alert(error.message);
+      return;
+    }
+
+    var $ = cheerio.load(body);
+
+    var categories = $('.menu-item-object-category').map(function () {
+      var url = $(this).find('a').attr('href').trim();
+      return {
+        title: $(this).find('a').first().text().trim(),
+        navigate: function () {
+          getArticles(url);
+        }
+      };
+    }).get();
+
+    shrimp.show({items: categories});
+  });
+}
+
+function getArticles(url) {
+  shrimp.request(url, function (error, response, body) {
+    if (error) {
+      shrimp.alert(error.message);
+      return;
+    }
+
+    var $ = cheerio.load(body);
+
+    var articles = $('.article article').map(function () {
+      var url = $(this).find('a').attr('href').trim();
+      return {
+        title: $(this).find('h2').text().trim(),
+        image: $(this).find('img').attr('src').trim(),
+        date: $(this).find('.thetime').text().trim(),
+        navigate: function () {
+          getArticle(url);
+        }
+      };
+    }).get();
+
+    var data = {items: articles};
+
+    if ($('.pagination .next').length > 0) {
+      var nextUrl = $('.pagination .next').attr('href');
+      data.next = function () {
+        getArticles(nextUrl);
+      };
+    }
+
+    shrimp.show(data);
+  });
+}
+
+function getArticle(url) {
+  shrimp.request(url, function (error, response, body) {
+    if (error) {
+      shrimp.alert(error.message);
+      return;
+    }
+
+    var $ = cheerio.load(body);
+
+    $('.ad-inserter').remove();
+    $('.wp-polls').remove();
+    $('.wp-polls-loading').remove();
+
+    var content = $('.post-single-content').html();
+    var article = {
+      title: $('h1.title').text().trim(),
+      content: content,
+      baseUrl: url
+    };
+
+    shrimp.show({item: article});
+  });
+}
+
+shrimp.setEntry(getCategories);
